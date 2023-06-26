1648846c7c5dcb73580ff302c773c75dfb53378b
y1007695527 <67174777+y1007695527@users.noreply.github.com>
2023-06-26 16:33:51 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,7 +120,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Build your own AI assistant,Customer service WeChat wuclound.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
